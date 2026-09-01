e364240506b881b48ac19093e86a68796ea12844
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-01 09:34:04 +0530

    Add files via upload

---
+++ loops2.java
@@ -0,0 +1,9 @@
+// print the numbers from 0 to 10
+
+public class loops2{
+    public static void main(String[] args){
+        for(int counter = 1; counter < 11; counter++){
+            System.out.println(counter);
+        }
+    }
+}
---
+++ whileloop1.java
@@ -0,0 +1,11 @@
+// print the numbers from 0 to 10 using while loop
+
+public class whileloop1{
+    public static void main(String[] args){
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+             i++;
+        }
+    }
+}

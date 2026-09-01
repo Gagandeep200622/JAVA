661375e5beecfb1bcec8a3090c8c083c30b0b7b2
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-01 09:32:52 +0530

    Add files via upload

---
+++ loops1.java
@@ -0,0 +1,9 @@
+// print hello world 3 times
+
+public class loops1{
+    public static void main(String[] args){
+        for(int counter = 0; counter < 3; counter++){
+            System.out.println("Hello World");
+        }
+    }
+}

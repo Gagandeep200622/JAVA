5c0bf3d2d258ae1ed2dae9135f6f762c96c00e5f
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-01 11:05:25 +0530

    Add files via upload

---
+++ practice2.java
@@ -0,0 +1,17 @@
+// Declare a  variable and print its value
+
+public class practice2{
+    public static void main(String[] args){
+        int age = 21;
+        double price = 20.20;
+        char alphabet = 'a';
+        boolean t = true;
+        String name = "GAGANDEEP";
+
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(alphabet);
+        System.out.println(t);
+        System.out.println(name);
+    }
+}

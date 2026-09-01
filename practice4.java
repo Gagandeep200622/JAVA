bb5789b15b5b39fcd8040bbbf6a5b6943d8b82c5
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-01 11:06:58 +0530

    Add files via upload

---
+++ practice4.java
@@ -0,0 +1,11 @@
+// write a program to calculate the area of rectangle
+
+public class practice4{
+    public static void main(String[] args){
+        int length = 10;
+        int width = 8;
+
+        int area = length * width;
+        System.out.println(area);
+    }
+}

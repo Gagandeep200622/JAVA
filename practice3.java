2c7adba46089a8262548a6a9bd8c033d3ca00ce9
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-01 11:06:30 +0530

    Add files via upload

---
+++ practice3.java
@@ -0,0 +1,21 @@
+
+// store two numbers a and print addition , subtraction, multiplication,division and Remainder
+
+public class practice3{
+    public static void main(String[] args){
+        int a = 21;
+        int b = 20;
+        int add = a + b;
+        int sub = a - b;
+        int multi = a * b;
+        float divide = a / b;
+        float reminder = a % b;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multi);
+        System.out.println(divide);
+        System.out.println(reminder);
+
+    }
+}

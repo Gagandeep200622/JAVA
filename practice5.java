278633fa5626c0ce8cab4377892bd3393293f7b7
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-01 11:07:39 +0530

    Add files via upload

---
+++ practice5.java
@@ -0,0 +1,14 @@
+// write a program to calculate the area of circle
+
+import java.util.*;
+
+public class practice5{
+    public static void main(String[] args){
+        
+        Scanner sc =new Scanner(System.in);
+        int r = sc.nextInt();
+
+        double area = 3.14 * r * r  ;
+        System.out.println(area);
+    }
+}

667c2f2441a239f671d0d67420ba6dd4e93f3e73
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-01 11:04:43 +0530

    Add files via upload

---
+++ practise1.java
@@ -0,0 +1,14 @@
+// print your name , age and college
+
+public class practise1{
+    public static void main(String[] args){
+        int age = 20;
+        String name = "Gagan";
+        String college = "Dayananda Sagear University";
+
+
+        System.out.println( name);
+        System.out.println(age);
+        System.out.println(college);
+    }
+}

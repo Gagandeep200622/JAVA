0b11076a061d05267a146ef2fd0b3633472376a4
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-03 07:51:46 +0530

    Add files via upload

---
+++ functions2.java
@@ -0,0 +1,18 @@
+// make a function to print the sum of to numbers and return the sum
+
+import java.util.*;
+
+public class fuctions2 {
+    public static int calculateSum(int a,int b){
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        int sum = calculateSum(a,b);
+        System.out.println(sum);
+    }
+
+}

955012c2bc595063e05cad2fbc95ee67446b7308
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-05 08:52:50 +0530

    Add files via upload

---
+++ practice01.java
@@ -0,0 +1,13 @@
+
+// Take a number and print its square and cube 
+
+import java.util.*;
+
+public class practice01{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(n * n);
+        System.out.println(n*n*n);
+    }
+}

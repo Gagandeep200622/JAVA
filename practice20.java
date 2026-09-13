c496b968b1e602a127c413ca9800fbe70a310806
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-13 11:57:28 +0530

    Add files via upload

---
+++ practice20.java
@@ -0,0 +1,15 @@
+//Find largest element in an array
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 7, 89, 23};
+        int largest = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + largest);
+    }
+}

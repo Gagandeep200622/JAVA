851532e3241cb3e97f1290f7f1ff9d2bd994bd67
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-14 21:22:16 +0530

    Add files via upload

---
+++ practice21.java
@@ -0,0 +1,17 @@
+//Search an element in an array
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+        boolean found = false;
+
+        for (int num : arr) {
+            if (num == target) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println(found ? "Found" : "Not Found");
+    }
+}

6036f9240d07a89a0e4c28439f5c92da7649ac5f
Gagandeep R Naik <gagannaik8050@gmail.com>
2026-09-16 20:03:20 +0530

    Add files via upload

---
+++ practice23.java
@@ -0,0 +1,18 @@
+//Product of digits
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int product = 1;
+
+        while (n != 0) {
+            product *= n % 10;
+            n /= 10;
+        }
+
+        System.out.println(product);
+    }
+}
